14fe547fe2dd48a2d7f1ba62c2202518ab5cdd4f
RajSingh <68813966+rajsingh-vks@users.noreply.github.com>
2020-08-12 00:18:18 +0530

    filter_redux
    Signed-off-by: RajSingh <68813966+rajsingh-vks@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import data from './data.json';
+// import data from './data.json';
 import Products from './components/Products';
 import Filter from './components/Filter';
 import Cart from './components/cart';
@@ -7,14 +7,22 @@ import store from './store/store/store';
 import { Provider } from 'react-redux';
 
 export class App extends Component {
+  // constructor(){
+  //   super();
+  //   this.state = {
+  //     products: data.products,
+  //     cartItems: localStorage.getItem('cartItems') ? 
+  //     JSON.parse(localStorage.getItem('cartItems')): [],
+  //     size: "",
+  //     sort: "",
+  //   };
+  // }
+
   constructor(){
     super();
     this.state = {
-      products: data.products,
       cartItems: localStorage.getItem('cartItems') ? 
       JSON.parse(localStorage.getItem('cartItems')): [],
-      size: "",
-      sort: "",
     };
   }
 
@@ -51,41 +59,41 @@ export class App extends Component {
   }
 
 
-  sortProducts = (event) => {
-      const sort = event.target.value;
-      console.log(event.target.value);
-      this.setState((state) => ({
-        sort: sort,
-        // Slice Is used to clone the items
-        products : this.state.products
-        .slice()
-        .sort((a,b) => 
-          sort === 'lowest'
-          ? a.price > b.price
-           ? 1
-           :-1
-           : sort === 'highest'
-           ? a.price < b.price 
-           ? 1:-1
-           : a._id < b._id 
-           ? 1:-1
-        ),
-      }));
-  }
+  // sortProducts = (event) => {
+  //     const sort = event.target.value;
+  //     console.log(event.target.value);
+  //     this.setState((state) => ({
+  //       sort: sort,
+  //       // Slice Is used to clone the items
+  //       products : this.state.products
+  //       .slice()
+  //       .sort((a,b) => 
+  //         sort === 'lowest'
+  //         ? a.price > b.price
+  //          ? 1
+  //          :-1
+  //          : sort === 'highest'
+  //          ? a.price < b.price 
+  //          ? 1:-1
+  //          : a._id < b._id 
+  //          ? 1:-1
+  //       ),
+  //     }));
+  // }
 
-  filterProducts = (event) => {
-    console.log(event.target.value);
-    if(event.target.value === '') {
-      this.setState({size:event.target.value, products:data.products});
-    } else {
-      this.setState({
-        size: event.target.value,
-        products: data.products.filter(
-          (product) => product.availableSizes.indexOf(event.target.value) >= 0
-        ),
-      });
-    }
-  }
+  // filterProducts = (event) => {
+  //   console.log(event.target.value);
+  //   if(event.target.value === '') {
+  //     this.setState({size:event.target.value, products:data.products});
+  //   } else {
+  //     this.setState({
+  //       size: event.target.value,
+  //       products: data.products.filter(
+  //         (product) => product.availableSizes.indexOf(event.target.value) >= 0
+  //       ),
+  //     });
+  //   }
+  // }
 
   render() {
   return (
@@ -97,7 +105,7 @@ export class App extends Component {
       <main>
         <div className='content'>
           <div className='main'>
-            <Filter 
+            {/* <Filter 
              count={this.state.products.length}
              size={this.state.size}
              sort={this.state.sort}
@@ -105,7 +113,9 @@ export class App extends Component {
              sortProducts={this.sortProducts}></Filter>
             <Products 
             products={this.state.products} 
-            addToCart={this.addToCart}></Products>
+            addToCart={this.addToCart}></Products> */}
+            <Filter/>
+            <Products addToCart={this.addToCart}></Products>
           </div>
           <div className='sidebar'>
             <Cart 
