90931764e574e59be866f4566a5b522988e0d35f
RajSingh <68813966+rajsingh-vks@users.noreply.github.com>
2020-08-15 20:34:26 +0530

    git comit
    Signed-off-by: RajSingh <68813966+rajsingh-vks@users.noreply.github.com>

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const shortid = require("shortid");
+const favicon = require('express-favicon');
 
 const app = express();
 app.use(bodyParser.json());
 
+app.use(favicon(__dirname + '/build/favicon.ico'));
 app.use('/', express.static(__dirname + '/build'));
 app.get('/', (req, res) => res.sendFile(__dirname + '/build/index.html'))
 
